361f44f3aafb36663c0ecdaa4333cd94ff34fee5
Eigil Fjeldgren Rischel <ayegill@gmail.com>
2019-05-22 11:42:28 +0200

    Implemented command files and a -v flag

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 Usage:
 
 `scrawl testfile.tex.scr > testfile.tex`
+
 `scrawl < testfile.tex.scr > testfile.tex`
+
+`scrawl -c testcmds testfile.tex.scr > testfile.tex`
+
+`scrawl -v testfile.tex.scr > testfile.tex`
 
 The syntax is as follows:
 Lines beginning with the triad `%-#` are called "code lines", and are interpreted by `scrawl` - all other lines are simply re-emitted.
@@ -14,6 +19,12 @@ The standard output is then emitted.
 
 Note that there's almost no error checking, so don't fuck up the rules.
 
+The `-v` flag provides some minor debugging output.
 
+You can provide a macro/command file with the `-c` flag.
+Each line on the command file must contain a command name with no spaces, followed by a space, followed by a command (which may contain spaces, but no newlines).
+A block starting with a name from the command file is run using the matching command.
 
 See the test file for an example.
+The test file can be built with `scrawl -c testcmds testfile.tex.scr > testfile.tex`.
+Note that this requires the [strid](https://github.com/smimram/strid) utility.
--- scrawl.go
+++ scrawl.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -14,6 +15,31 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func getCommand(cmd string, cmdpath string) string {
+	//Runs through each line of cmdpath.
+	//If the first word of the line (ended by a space) is cmd,
+	//return the rest of line, without the space.
+	//If no matches, return cmd.
+
+	if cmdpath == "" {
+		return cmd
+	}
+
+	contents, err := ioutil.ReadFile(cmdpath)
+	check(err)
+	for _, line := range strings.Split(string(contents), "\n") {
+		split := strings.SplitN(line, " ", 2)
+		if len(split) == 0 {
+			io.WriteString(os.Stderr, "Empty line in command file\n")
+		} else if len(split) == 1 {
+			io.WriteString(os.Stderr, "Line in command file without spaces\n")
+		} else if split[0] == cmd { //Found a proper split, we're good to go.
+			return split[1]
+		}
+	}
+	return cmd
 }
 
 func run(command string, input string) string {
@@ -33,7 +59,7 @@ func run(command string, input string) string {
 	return string(output)
 }
 
-func doFile(file *os.File) {
+func doFile(file *os.File, cmdfile string) {
 
 	scanner := bufio.NewScanner(file)
 
@@ -45,7 +71,7 @@ func doFile(file *os.File) {
 		if len(line) > 2 && line[:3] == "%-#" { //we are reading scrawl input. Continue.
 			if inCmd { //Continue parsing command input
 				if len(line) > 5 && line[3:6] == "---" { //Command is over, execute it.
-					output := run(command, strings.Join(input[:], "\n"))
+					output := run(getCommand(command, cmdfile), strings.Join(input[:], "\n"))
 					inCmd = false
 					command = ""
 					input = make([]string, 0)
@@ -63,17 +89,29 @@ func doFile(file *os.File) {
 	}
 }
 
+func errmsg(msg string, v bool) {
+	if v {
+		io.WriteString(os.Stderr, msg)
+	}
+
+}
+
 func main() {
-	if len(os.Args) == 0 {
-		io.WriteString(os.Stderr, "no args")
-		doFile(os.Stdin)
+	verbose := flag.Bool("v", false, "Print debugging information to stderr")
+	cmdfile := flag.String("c", "", "Load a macro file - see README or source code for info")
+
+	flag.Parse()
+
+	if len(flag.Args()) == 0 {
+		errmsg("No args\n", *verbose)
+		doFile(os.Stdin, *cmdfile)
 	} else {
-		io.WriteString(os.Stderr, "Some args")
-		for _, path := range os.Args[1:] {
-			io.WriteString(os.Stderr, path)
+		errmsg("Some args\n", *verbose)
+		for _, path := range flag.Args() {
+			errmsg(path, *verbose)
 			file, err := os.Open(path)
 			check(err)
-			doFile(file)
+			doFile(file, *cmdfile)
 		}
 	}
 }
